1935908913f9d657ce84147d2e3dded5789240e5
Jonathan Braat <jonathan.braat@live.com>
2021-12-14 16:22:14 +1300

    [ADD] portfolio performance parse cash activities

--- src/helper/index.js
+++ src/helper/index.js
@@ -79,7 +79,7 @@ export function findPreviousRegexMatchIdx(arr, idx, regex) {
   return -1;
 }
 
-export function validateActivity(activity, findSecurityAlsoByCompany = false) {
+function validateCommons(activity) {
   // All fields must have a value unequal undefined
   if (!every(values(activity), a => !!a || a === 0)) {
     console.error(
@@ -95,15 +95,6 @@ export function validateActivity(activity, findSecurityAlsoByCompany = false) {
 
   const oldestDate = new Date(1990, 1, 1);
   oldestDate.setUTCHours(0, 0, 0, 0);
-
-  // The date property must be present.
-  if (activity.date === undefined) {
-    console.error(
-      'The activity date for ' + activity.broker + ' must be present.',
-      activity
-    );
-    return undefined;
-  }
 
   // The datetime property must be present.
   if (activity.datetime === undefined) {
@@ -204,6 +195,21 @@ export function validateActivity(activity, findSecurityAlsoByCompany = false) {
     return undefined;
   }
 
+  return activity;
+}
+
+export function validateActivity(activity, findSecurityAlsoByCompany = false) {
+  if(validateCommons(activity) === undefined) return undefined;
+
+  // The date property must be present.
+  if (activity.date === undefined) {
+    console.error(
+      'The activity date for ' + activity.broker + ' must be present.',
+      activity
+    );
+    return undefined;
+  }
+
   // Tresor One will search the security for PDF Documents with ISIN or WKN. For Imports of .csv File from Portfolio Performance
   // T1 can search the security also by the Company.
   if (
@@ -244,9 +250,7 @@ export function validateActivity(activity, findSecurityAlsoByCompany = false) {
   }
 
   if (
-    !['Buy', 'Sell', 'Dividend', 'TransferIn', 'TransferOut'].includes(
-      activity.type
-    )
+    !['Buy', 'Sell', 'Dividend', 'TransferIn', 'TransferOut'].includes(activity.type)
   ) {
     console.error(
       'The activity type for ' +
@@ -258,6 +262,10 @@ export function validateActivity(activity, findSecurityAlsoByCompany = false) {
   }
 
   return activity;
+}
+
+export function validateCashActivity(activity) {
+  return validateCommons(activity);
 }
 
 // Finds next regex match starting at the given offset
